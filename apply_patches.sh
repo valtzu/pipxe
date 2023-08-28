16efdeb73a0f41fc7e0492c2d27c904dd710b7d7
valtzu <valtzu@gmail.com>
2023-08-28 20:54:53 +0300

    Make sure git config is set when applying patches

--- apply_patches.sh
+++ apply_patches.sh
@@ -7,6 +7,8 @@ cd $(dirname $0)
 for dir in patches/* ; do
   repo=$(basename $dir)
   pushd $repo
+  git config user.email || git config user.email "nobody@example.org"
+  git config user.name || git config user.name "Nobody"
   for patch in ../patches/$repo/*.patch ; do
     git apply --3way --ignore-space-change $patch
     git commit -m "Apply $(basename $patch)"
